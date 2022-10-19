f0dc8e3ed29fb188b0e548456c937220c95eb5b5
Kohize <kohi881@gmail.com>
2022-10-19 19:32:02 +0300

    added js modules

---
+++ js/data.js
@@ -0,0 +1,17 @@
+import { getRandomIntInclusive } from './util.js';
+
+const PICTURE_NUMBER = 25;
+const pictureInfo = function (index) {
+  return {
+    id: index,
+    url: `photos/${index}.jpg`,
+    description: 'Отправлено через Кекстаграм',
+    likes: getRandomIntInclusive(15, 200),
+    comments: getRandomIntInclusive(0, 200),
+  };
+};
+
+const CREATE_PICTURE = Array.from({ length: PICTURE_NUMBER }, (_, infoIndex) => pictureInfo(infoIndex + 1));
+
+
+export { CREATE_PICTURE };
---
+++ js/util.js
@@ -0,0 +1,19 @@
+//Функция, возвращающая случайное целое число из переданного диапазона включительно.
+//https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+function getRandomIntInclusive(min, max) {
+  if (min < 0 || max < 0 || min === max || min > max) {
+    return NaN;
+  }
+  else {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+}
+getRandomIntInclusive(5, 30);
+
+//Функция для проверки максимальной длины строки.
+function textLength(string, maxLength) {
+  return string.length <= maxLength;
+}
+textLength('HtmlAcademy', 20);
+
+export { getRandomIntInclusive, textLength };
